550721a23abdc1781689a2f16f3c6cdac5b6d3bb
Joseph Shapiro <jdshap@comcast.net>
2022-07-13 12:40:19 -0400

    Initial layout

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
+
+      <body className="App-body">
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </body>
+
+      <footer className="App-footer">
+        <div style={{"width":"70%"}}>something<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>Something else</div>
+      </footer>
     </div>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,21 @@
+import './navbar.css';
+
+import React from "react";
+
+import Navtab from '../navtab';
+
 function Navbar() {
-    return <img src="/logo.svg" alt="logo.svg" style={{"width": "128px", "height": "128px"}} />;
+    return (
+        <div className="Navbar">
+            <img src="/logo.svg" alt="logo.svg" style={{"width": "40px", "height": "40px", "filter":"grayscale(100%) brightness(1000%)"}} />
+            <div style={{"font-weight":"bold"}}>Polyfabrica Technologies, LLC</div>
+            <div className="Navbar-tabs">
+                <Navtab name="Software" reference="null" />
+                <Navtab name="3D Printing" reference="null" />
+                <Navtab name="About" reference="null" />
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
