ae6918b21ae5cabb219ada63c586a0b639477892
Joseph Shapiro <jdshap@comcast.net>
2023-08-30 15:20:09 -0400

    Adjusted for resolution edge cases

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,8 +7,10 @@ import Navtab from '../navtab';
 function Navbar() {
     return (
         <div className="Navbar">
-            <a href='/'><img src="/logo.svg" alt="logo.svg" style={{"width": "2em", "height": "2em", "padding":"0", "filter":"grayscale(100%) brightness(1000%)"}} /></a>
-            <div className="Title" style={{"font-weight":"bold"}}>Joseph Shapiro</div>
+            <a href='/'>
+                <img src="/logo.svg" alt="logo.svg" style={{"width": "2em", "height": "2em", "padding":"0", "filter":"grayscale(100%) brightness(1000%)"}} />
+                <span className="Title" style={{"font-weight":"bold"}}>Joseph Shapiro</span>
+            </a>
             <div className="Navbar-tabs">
                 <Navtab name="Software" reference="/software" />
                 <Navtab name="3D Printing" reference="printing" />
